76d3edc30d180d131b0ef96edd9f04e7ecaeba4d
Manue-777 <manuel.morales.avn@gmail.com>
2025-09-09 13:31:08 -0600

    Subiendo mi página CATRA

---
+++ admin/Data.js
@@ -0,0 +1,23 @@
+const constanciasData = {
+    
+    "CATRA-2025-00147123": {
+        nombre: "MANUEL MORALES AVENDAÑO",
+        curp: "MOAM010914HOCRVNA1",
+        curso: "APLICACION DE TOXINA BOTULINICA",
+        duracion: "3 HORAS",
+        calificacion: "9.8 / 10.0",
+        fecha: "09 DE SEPTIEMBRE DE 2025",
+        folio: "CATRA-2025-00147123"
+    },
+    
+    "CATRA-2025-00147124": {
+        nombre: "EMMANUEL AVENDAÑO MORALES",
+        curp: "MOAM010914HOCRVNA1",
+        curso: "APLICACION DE TOXINA BOTULINICA",
+        duracion: "20 HORAS",
+        calificacion: "9.8 / 10.0",
+        fecha: "09 DE SEPTIEMBRE DE 2025",
+        folio: "CATRA-2025-00147124"
+    },
+   
+};
