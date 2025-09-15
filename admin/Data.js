e526e4515e7bac3fc4aa6eb4880695935e05eb19
capacitacioncatra <capacitacioncatra@gmail.com>
2025-09-15 11:04:38 -0600

    Base de datos actualizada

--- admin/Data.js
+++ admin/Data.js
@@ -10,6 +10,15 @@ const constanciasData = {
         folio: "CATRA-2025-00147123",
         instructorId: "inst001" // ID del instructor de CATRA
     },
-    
+    "CATRA-2025-00147124": {
+        nombre: "JENIFER GUADALUPE JUAREZ GOMEZ",
+        curp: "JUGJ031211MOCRMNA8",
+        curso: "MANEJO CONTRA EL RIESGO",
+        duracion: "8 HORAS",
+        calificacion: "9.8 / 10.0",
+        fecha: "09 DE SEPTIEMBRE DE 2025",
+        folio: "CATRA-2025-00147124",
+        instructorId: "inst002" // ID del instructor de CATRA
+    },
 
 };
